582379a2e62da78d37fc4aeb39224dfae867c0c7
Asalle <asalle.kim@gmail.com>
2017-11-26 19:05:32 +0100

    Finish the Prettify.hs

--- src/PrettyJson.hs
+++ src/PrettyJson.hs
@@ -4,7 +4,7 @@ module PrettyJson
     )
     where
         import SimpleJson
-        import PrettyStub
+        import Prettify
 
         renderJValue :: JValue -> Doc
         renderJValue (JBool True)  = text "true"
@@ -12,3 +12,12 @@ module PrettyJson
         renderJValue (JNull) = text "null"
         renderJValue (JString s) = string s
         renderJValue (JNumber n) = double n
+        renderJValue (JArray a) = series '[' ']' renderJValue a
+        renderJValue (JObject o) = series '{' '}' field o
+            where field (name, val) = string name <> text ": " <> renderJValue val
+
+        series :: Char -> Char -> (a -> Doc) -> [a] -> Doc
+        series left right printFunc = enclose left right . fsep . punctuate (char ',') . map printFunc
+
+
+

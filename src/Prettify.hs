d972551d921714d3bcd26583d08828aec21d8de5
Asalle <asalle.kim@gmail.com>
2017-11-27 15:48:11 +0100

    Add final pretty formatters

--- src/Prettify.hs
+++ src/Prettify.hs
@@ -11,6 +11,8 @@ Portability :  portable
 module Prettify
     (
         Doc
+      , pretty
+      , compact
       , string
       , text
       , double
@@ -34,6 +36,40 @@ module Prettify
                 | Concat Doc Doc
                 | Union Doc Doc
             deriving (Show, Eq)
+
+        pretty width x = best 0 [x]
+            where best col (d:ds) =
+                      case d of
+                        Empty        -> best col ds
+                        Char c       -> c :  best (col + 1) ds
+                        Text s       -> s ++ best (col + length s) ds
+                        Line         -> '\n' : best 0 ds
+                        a `Concat` b -> best col (a:b:ds)
+                        a `Union` b  -> nicest col (best col (a:ds))
+                                                   (best col (b:ds))
+                  best _ _ = ""
+
+                  nicest col a b | (width - least) `fits` a = a
+                                 | otherwise                = b
+                                 where least = min width col
+
+        compact :: Doc -> String
+        compact x = transform [x]
+            where transform [] = ""
+                  transform (x:xs) =
+                    case x of
+                        Empty -> transform xs
+                        Char c -> c : transform xs
+                        Text s -> s ++ transform xs
+                        Line   -> '\n' : transform xs
+                        a `Concat` b -> transform (a:b:xs)
+                        _ `Union` b -> transform (b:xs)
+
+        fits :: Int -> String -> Bool
+        w `fits` _ | w < 0 = False
+        w `fits` ""        = True
+        w `fits` ('\n':_)  = True
+        w `fits` (c:cs)    = (w - 1) `fits` cs
 
         -- | 'string' pretty prints a string adding quotes
         string :: String -> Doc
